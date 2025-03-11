19ff7cf5b45b52fd967ebfb10e728a21d1a0104d
arjav0703 <arjavjain0703@gmail.com>
2025-03-11 13:09:51 +0530

    add initials function

--- 1.Basics/index.js
+++ 1.Basics/index.js
@@ -2,9 +2,21 @@
 
 console.log('Hello, World!')
 
-let name = 'AJ';
-console.log(name);
+//let name = 'AJ';
+//console.log(name);
 
 //console.log(4 + 3)
 //console.log('10' === 10)
-console.log( 4 !== "4" )
+//console.log( 4 !== "4" )
+    
+//const firstName = 'Arjav'
+//const lastName = 'Jain'
+
+const firstName = prompt("What's your first name?");
+const lastName = prompt("What's your last name")
+
+console.log(initials(firstName,lastName) + 'can be used as your initial!')
+
+function initials(firstName, lastName) {
+    return firstName.slice(0,1) + lastName.slice(0,1);
+}
