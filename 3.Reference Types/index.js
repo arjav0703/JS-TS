de296c87cdac6e222247d5837736893519263abe
arjav0703 <arjavjain0703@gmail.com>
2025-03-11 16:15:31 +0530

    create an object

--- 3.Reference Types/index.js
+++ 3.Reference Types/index.js
@@ -1,3 +1,4 @@
+// ARRAYS
 /*
 const strArray = ['eat', 'sleep', 'code', 'repeat'];
 let i= 0;
@@ -6,3 +7,18 @@ do {
   i++;
 } while (i < strArray.length);
 */
+
+// OBJECTS
+
+const person = {
+    Firstname: 'John',
+    Lastname: 'Wick',
+    age: 40,
+    city: 'New York',
+    proffession: 'Assassin',
+    company: 'High Table',
+    aka: 'The Boogeyman',
+    isretired: false,
+};
+
+console.log(person.Firstname + ' ' + person.Lastname);
