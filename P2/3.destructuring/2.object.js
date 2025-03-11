f6087be59231ecf2ce99b2e1fe067b2aefe019f2
arjav0703 <arjavjain0703@gmail.com>
2025-03-11 20:45:11 +0530

    add challenge statement

--- P2/3.destructuring/2.object.js
+++ P2/3.destructuring/2.object.js
@@ -1,11 +1,24 @@
 // Object destructuring
 // Note, in object destructuring, the variable name must match the key name in the object. order does not matter.
-const person = {
-    name: 'Jared',
-    age: 30,
-    isHackclubber: true,
-    work: 'Club Operations Lead'
-};
+// const person = {
+//     userName: 'Jared',
+//     age: 30,
+//     isHackclubber: true,
+//     work: 'Club Operations Lead'
+// };
 
-const {name, work} = person;
-console.log(`${name} is the ${work} at Hackclub.`);
+// const {name, work} = person;
+// console.log(`${userName} is the ${work} at Hackclub.`);
+
+
+// Challenge
+
+const person = {
+    name: "John Doe",
+    age: 30,
+    gender: "male",
+    country: "USA",
+  };
+  
+  // Your task is to use object destructuring to extract the name, age, and country properties from the person object.
+  // After extracting the properties, log each variable's value to the console.
