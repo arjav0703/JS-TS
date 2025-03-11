f37310e3bd66539bdd2aaa480eba9a40da796945
arjav0703 <arjavjain0703@gmail.com>
2025-03-11 13:30:45 +0530

    mkdir control flow && touch index.html && touch index.js

--- 1.Basics/index.js
+++ 1.Basics/index.js
@@ -13,10 +13,11 @@ console.log('Hello, World!')
 //const lastName = 'Jain'
 
 const fullName = prompt("What's your name?");
-let [firstName, lastName] = fullName.split(' ')
+let [firstName, lastName] = fullName.split(' ');
 
 //console.log(firstName)
-console.log(initials(firstName,lastName) + ' can be used as your initial!')
+console.log(initials(firstName.toUpperCase(),lastName.toUpperCase()) + ' can be used as your initial!');
+
 
 function initials(firstName, lastName) {
     return firstName.slice(0,1) + lastName.slice(0,1);
