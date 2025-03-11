cfa3e65a40305c673a81c2a686dbbf0abe7b23bb
arjav0703 <arjavjain0703@gmail.com>
2025-03-11 14:01:52 +0530

    ahh! never heard of that fruit :(

--- 2. Control Flow/index.js
+++ 2. Control Flow/index.js
@@ -1,3 +1,4 @@
+/*
 let password; 
 
 password = prompt('Set a password between 8 to 12 characters')
@@ -11,4 +12,18 @@ if (password.length < 8) {
     console.log('Password too long')
 } else {
     console.log('Password created succesfully!')
+}
+*/
+
+let fruit = 'Apple';
+
+switch (fruit.toLowerCase) {
+    case 'apple':
+        console.log('How you like them apples?')
+    case 'orange':
+        console.log('Not a fan of oranges')
+    case 'banana':
+        console.log('Bananas are kinda mid')
+    default:
+        console.log('ahh! never heard of that fruit :(')
 }
