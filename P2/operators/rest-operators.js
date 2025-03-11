d250460d98ade01b82d177e21a549f59baf468b0
arjav0703 <arjavjain0703@gmail.com>
2025-03-11 19:58:37 +0530

    solve rest operator challenge!

--- P2/operators/rest-operators.js
+++ P2/operators/rest-operators.js
@@ -1,3 +1,10 @@
 // Challenge 
 // 1. Create a function name (sum) which takes (unlimited) amount of parameters
 // 2. No matter how many parameters you provide it will return the sum value of those params!
+
+// -----------------------------
+const infinityStoneColors = (...colors) => {
+    console.log(...colors);
+}
+
+infinityStoneColors('red', 'blue', 'green', 'yellow', 'orange', 'purple', 'black');
