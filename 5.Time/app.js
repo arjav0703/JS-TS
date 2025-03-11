8676f8db1737647d6d2b4fcfa682f1e5be5ecb12
arjav0703 <arjavjain0703@gmail.com>
2025-03-11 18:14:27 +0530

    fix the 4th dimention

--- 5.Time/app.js
+++ 5.Time/app.js
@@ -1,2 +1,3 @@
-const currentTime = new Date();
-console.log(currentTime);   
+const date = new Date();
+
+console.log( `the date is ${date.getDate()} th of ${date.getMonth() + 1} in ${date.getFullYear()}`); 
