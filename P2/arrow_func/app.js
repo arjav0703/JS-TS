04116e657b8e3d3620084cc1745f9f5d6ba1d441
arjav0703 <arjavjain0703@gmail.com>
2025-03-11 19:18:33 +0530

    solve arrow func challenge

--- P2/arrow_func/app.js
+++ P2/arrow_func/app.js
@@ -1,5 +1,23 @@
-greet = (name) => {
-    return `Howdy mate, I'm ${name}!`;
-}
+// greet = (name) => {
+//     return `Howdy mate, I'm ${name}!`;
+// }
 
-console.log(greet('John'))
+// console.log(greet('John'))
+
+// Challenge
+
+setTimeout(() => {
+  console.log("Hello");
+  setTimeout(() => {
+    console.log("Hey");
+    setTimeout(() => {
+      console.log("Namaste");
+      setTimeout(() => {
+        console.log("Hi");
+        setTimeout(() => {
+          console.log("Bonjour");
+        }, 2000);
+      }, 2000);
+    }, 2000);
+  }, 2000);
+}, 2000);
