c1410371912a7801102fe85ba9a92d45accfbda8
arjav0703 <arjavjain0703@gmail.com>
2025-03-11 19:56:13 +0530

    add challenge statement;

---
+++ P2/operators/rest-operators.js
@@ -0,0 +1,3 @@
+// Challenge 
+// 1. Create a function name (sum) which takes (unlimited) amount of parameters
+// 2. No matter how many parameters you provide it will return the sum value of those params!
