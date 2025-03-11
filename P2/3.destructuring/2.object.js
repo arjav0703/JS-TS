d974d8145cddefc18dc8529ea3d07ba09ff18dbe
arjav0703 <arjavjain0703@gmail.com>
2025-03-11 20:42:42 +0530

    hmm

--- P2/3.destructuring/2.object.js
+++ P2/3.destructuring/2.object.js
@@ -1,1 +1,10 @@
 // Object destructuring
+const person = {
+    name: 'Jared',
+    age: 30,
+    isHackclubber: true,
+    work: 'Club Operations Lead'
+};
+
+const {name, work} = person;
+console.log(`${name} is the ${work} at Hackclub.`);
