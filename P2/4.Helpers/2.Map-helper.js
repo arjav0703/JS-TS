be38249f0b252e11261e2762755fcc27382bc1b6
arjav0703 <arjavjain0703@gmail.com>
2025-03-12 17:10:05 +0530

    fiz files

--- P2/4.Helpers/2.Map-helper.js
+++ P2/4.Helpers/2.Map-helper.js
@@ -9,30 +9,3 @@ const result = arr.map(tenx = arr => arr * 10)
 console.log(result)
 
 
-const specs = [
-    { ram: 4, hdd: 100},
-    { ram: 8, hdd: 200},
-    { ram: 16, hdd: 500},
-    { ram: 32, hdd: 1000}
-]
-
-console.log(specs.filter(res => res.hdd >= 500))
-
-
-// Challenge : Filter the numbers over 18 (adult)
-const ages = [32, 33, 16, 40];
-
-console.log( ages.filter(num => num > 18))
-
-
-// Challenge 2
-const words = [
-    "spray",
-    "limit",
-    "elite",
-    "exuberant",
-    "destruction",
-    "present",
-  ];
-
-const res = words.filter(word => word.length > 6)
