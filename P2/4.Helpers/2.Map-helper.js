0104031ffec4c1281dff3c1392a0edd8bfae7b8e
arjav0703 <arjavjain0703@gmail.com>
2025-03-12 16:26:17 +0530

    solve challenge 2

--- P2/4.Helpers/2.Map-helper.js
+++ P2/4.Helpers/2.Map-helper.js
@@ -23,3 +23,16 @@ console.log(specs.filter(res => res.hdd >= 500))
 const ages = [32, 33, 16, 40];
 
 console.log( ages.filter(num => num > 18))
+
+
+// Challenge 2
+const words = [
+    "spray",
+    "limit",
+    "elite",
+    "exuberant",
+    "destruction",
+    "present",
+  ];
+
+const res = words.filter(word => word.length > 6)
