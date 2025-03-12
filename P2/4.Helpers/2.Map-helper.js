8165152f5d73abd2ea77bd6d0e8a9746115d974a
arjav0703 <arjavjain0703@gmail.com>
2025-03-12 16:21:14 +0530

    add exapmles to map helper

--- P2/4.Helpers/2.Map-helper.js
+++ P2/4.Helpers/2.Map-helper.js
@@ -7,3 +7,13 @@ arr = [3,6,7,2,6,8,1]
 const result = arr.map(tenx = arr => arr * 10)
 
 console.log(result)
+
+
+const specs = [
+    { ram: 4, hdd: 100},
+    { ram: 8, hdd: 200},
+    { ram: 16, hdd: 500},
+    { ram: 32, hdd: 1000}
+]
+
+console.log(specs.filter(res => res.hdd >= 500))
