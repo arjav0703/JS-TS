04e19cec18703e0cf7b555bd143e120d1389a32a
arjav0703 <arjavjain0703@gmail.com>
2025-03-12 12:52:44 +0530

    add notes

---
+++ 4.ternary-operator.js
@@ -0,0 +1,6 @@
+// Ternary Operator
+
+// Ternary Operator is a shorthand for if-else statement. It is a conditional operator that assigns a value to a variable based on some condition.
+// Syntax:
+// condition ? ifTrueExpr : elseExpr
+
