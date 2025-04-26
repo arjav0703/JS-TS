1a2e2e036bf89bdf4ad77b8df34c894611e7bbb1
Arjav Jain <arjavjain0703@gmail.com>
2025-04-26 14:45:51 +0530

    setup dev env

---
+++ Typescript/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// let array: any[];
+const vidisha = {
+    name: "Vidisha",
+    postalcode: 464001,
+    population: undefined,
+    state: "Madhya Pradesh",
+};
+console.log(`The city of ${vidisha.name} (${vidisha.postalcode}) is in the state of ${vidisha.state}`);
--- Typescript/index.js
+++
@@ -1,8 +0,0 @@
-// let array: any[];
-var vidisha = {
-    name: "Vidisha",
-    postalcode: 464001,
-    population: undefined,
-    state: "Madhya Pradesh",
-};
-console.log("The city of ".concat(vidisha.name, " (").concat(vidisha.postalcode, ") is in the state of ").concat(vidisha.state));
