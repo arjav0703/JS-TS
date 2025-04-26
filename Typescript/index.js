db3cf315a2da5d4e5739dce0071c59f58f3daa04
Arjav Jain <arjavjain0703@gmail.com>
2025-04-26 14:06:25 +0530

    this is intersting

--- Typescript/index.js
+++ Typescript/index.js
@@ -1,4 +1,21 @@
-var a = 10;
-var b = 100;
-a = 1;
-console.log(a, b);
+// let array: any[];
+// array = [1, 4, 4, 1, 7, 7, 4, 1, 4, 6, 9, 0, 3, 5, 7, 9, 6, 2];
+// console.log(array);
+// enum mpos {
+//   x = 0,
+//   y = 0,
+// }
+// let myvar: string | null;
+// myvar = "Hello";
+// console.log(myvar.length);
+var typefunc;
+typefunc = function (x) {
+    if (typeof x === "string")
+        console.log(x.toUpperCase(), console.log(typeof x));
+    else if (typeof x === "number")
+        console.log(x.toString(), console.log(typeof x));
+    else
+        console.log("the value is null");
+};
+var variable = 23;
+typefunc(variable);
--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,4 +1,26 @@
-let a = 10;
-const b = 100;
-a = 1;
-console.log(a, b);
+// let array: any[];
+
+// array = [1, 4, 4, 1, 7, 7, 4, 1, 4, 6, 9, 0, 3, 5, 7, 9, 6, 2];
+// console.log(array);
+
+// enum mpos {
+//   x = 0,
+//   y = 0,
+// }
+
+// let myvar: string | null;
+
+// myvar = "Hello";
+// console.log(myvar.length);
+
+let typefunc: Function;
+typefunc = (x: string | number | null) => {
+  if (typeof x === "string")
+    console.log(x.toUpperCase(), console.log(typeof x));
+  else if (typeof x === "number")
+    console.log(x.toString(), console.log(typeof x));
+  else console.log("the value is null");
+};
+
+const variable = 23;
+typefunc(variable);
